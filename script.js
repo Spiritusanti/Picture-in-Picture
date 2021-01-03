4c8b01f37ca3324570e9303df06ad4e81975741f
Spiritusanti <mccrackenjacob@gmail.com>
2021-01-03 10:15:27 -0800

    full project using picture in picture web API

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+// Prompt user to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // catch error here
+        console.log('error: ' + error);
+    }
+}
+
+button.addEventListener('click', async() =>{
+    // Disable Button
+    button.disabled = true;
+    // start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // reset button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
